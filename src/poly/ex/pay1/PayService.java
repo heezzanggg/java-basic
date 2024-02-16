be0f01a5e4c1b6b66a9bf48c9fd6a0be835572d2
hee-window <heezzanggg@gmail.com>
2024-02-16 10:37:22 +0900

    section12.다형성과 설계 - example(pay_system)

---
+++ src/poly/ex/pay1/PayService.java
@@ -0,0 +1,14 @@
+package poly.ex.pay1;
+
+public class PayService {
+    public void processPay(String option, int amount) {
+        Pay pay = PayStore.findPay(option);
+        boolean result = pay.pay(amount);
+
+        if (result) {
+            System.out.println("결제 성공");
+        } else {
+            System.out.println("결제 실패");
+        }
+    }
+}
